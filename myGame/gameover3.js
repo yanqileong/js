a052a083e8cc97f1c4212f95e22d55b7a3349691
yanqileong <yanqileong@gmail.com>
2025-11-18 13:58:22 +0800

    Add files via upload

---
+++ myGame/gameover3.js
@@ -0,0 +1,59 @@
+class gameover3 extends Phaser.Scene {
+  constructor() {
+    super({
+      key: "gameover3",
+    });
+        // Put global variable here
+}
+
+   preload() {
+        // load image
+        this.load.image("gameoverImg", "assets/gameover.png");
+    }
+
+
+    create() {
+      console.log("*** gameover3 scene");
+
+         window.heart = 3;
+
+           // Check for spacebar or any key here
+        var spaceDown = this.input.keyboard.addKey('SPACE');
+
+               // On spacebar event, restart game     
+        spaceDown.on('down', function () {
+            console.log('restart room2');
+
+            this.scene.start('room2',
+                // Optional parameters
+                {
+
+                }
+            );
+        }, this)
+        
+
+    // The image is automatically scaled to fit the frame
+    let imgWidth = 2888;
+    let imgHeight = 2668;
+    let scaleX = this.cameras.main.width / imgWidth;
+    let scaleY = this.cameras.main.height / imgHeight;
+    let finalScale = Math.min(scaleX, scaleY);
+
+        this.add.image(
+      this.cameras.main.centerX,
+      this.cameras.main.centerY,
+      "gameoverImg"
+    )
+      .setOrigin(0.5)
+      .setScale(finalScale);
+
+        // Prevent pressing keys too quickly.
+        this.restartCooldown = false;    
+
+    }
+    
+  update() {
+  }//////////// end of class update /////////////////
+
+}//////////// end of class gameover3 /////////////////
